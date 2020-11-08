6d350f05d0c7a94a4ef4b785bf887b75bda83ecb
LebogangBantsijang <l.bantsijangthefirst@gmail.com>
2020-11-08 19:32:44 +0200

    initial commit

---
+++ app/src/main/java/com/lebogang/akira/PhoneSignInActivity.kt
@@ -0,0 +1,11 @@
+package com.lebogang.akira
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhoneSignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone_sign_in)
+    }
+}
