d724dcfc8458c849f0b51a4774a48073c506f672
LebogangBantsijang <l.bantsijangthefirst@gmail.com>
2020-11-12 12:37:57 +0200

    phone verification functionality

--- app/src/main/java/com/lebogang/akira/PhoneSignInActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.lebogang.akira
-
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.core.widget.doOnTextChanged
-import com.lebogang.akira.databinding.ActivityPhoneSignInBinding
-
