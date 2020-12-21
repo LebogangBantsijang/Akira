f7050532a9bc1713a9b12a9d9d3f62dd5ba8d60e
LebogangBantsijang <l.bantsijangthefirst@gmail.com>
2020-12-21 18:50:08 +0200

    commit

--- app/src/main/java/com/lebogang/akira/MainActivity.kt
+++ app/src/main/java/com/lebogang/akira/MainActivity.kt
@@ -15,42 +15,38 @@ package com.lebogang.akira
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.facebook.AccessToken
-import com.facebook.CallbackManager
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.common.api.ApiException
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.lebogang.akira.EmailPackage.EmailSignInActivity
+import com.lebogang.akira.EmailPackage.EmailSignUpActivity
 import com.lebogang.akira.FacebookPackage.FacebookSignInObject
 import com.lebogang.akira.GooglePackage.GoogleSignInObject
-import com.lebogang.akira.PhonePackage.PhoneSignInActivity
-import com.lebogang.akira.databinding.ActivityMain2Binding
+import com.lebogang.akira.PhonePackage.PhoneActivity
+import com.lebogang.akira.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding:ActivityMain2Binding
-    private lateinit var googleSignInObject: GoogleSignInObject
-    private lateinit var facebookSignInObject: FacebookSignInObject
+    private val binding :ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private val googleSignInObject:GoogleSignInObject by lazy {
+        GoogleSignInObject(this)
+    }
+    private val facebookSignInObject: FacebookSignInObject by lazy {
+        FacebookSignInObject(this)
+    }
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
         checkIfUserIsSignedIn()
-        googleSignInObject = GoogleSignInObject(this)
-        facebookSignInObject = FacebookSignInObject(this)
         setupButtons()
     }
 
-    //initialise sign in buttons
     private fun setupButtons(){
         binding.phoneButton.setOnClickListener {
-            startActivity(Intent(this, PhoneSignInActivity::class.java))
+            startActivity(Intent(this, PhoneActivity::class.java))
         }
         binding.emailButton.setOnClickListener {
-            startActivity(Intent(this, EmailSignInActivity::class.java))
+            startActivity(Intent(this, EmailSignUpActivity::class.java))
         }
         binding.googleButton.setOnClickListener {
             googleSignInObject.signIn()
@@ -69,7 +65,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        //googleSignInObject!!.onActivityResult(data)
         if (requestCode == googleSignInObject.requestCode){
             googleSignInObject.onActivityResult(data)
         } else facebookSignInObject.onActivityResult(requestCode,resultCode,data)
